ba2c5f690f451935918022955e4cae23f7c0a15e
Rocío Tovar <rocio.tovar@worldline.com>
2016-02-16 08:36:17 +0100

    Added empty tabbar project.

---
+++ WondyDemo/WondyDemo/src/ui/vc/WDCenterListVC.swift
@@ -0,0 +1,28 @@
+//
+//  WDCenterListVC.swift
+//  WondyDemo
+//
+//  Created by Rocío Tovar on 16/02/16.
+//  Copyright © 2016 Rocío Tovar. All rights reserved.
+//
+
+import UIKit
+
+class WDCenterListVC: UIViewController {
+
+    @IBOutlet weak var noContentView: UIView!
+    @IBOutlet weak var centersStackView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
